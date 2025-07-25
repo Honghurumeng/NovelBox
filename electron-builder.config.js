16a484c7a7b6d1b175c9f7bb3ff558eacfaee1a0
AliyahZombie <curaalizm@gmail.com>
2025-07-26 02:46:02 +0800

    ci

---
+++ electron-builder.config.js
@@ -0,0 +1,63 @@
+/**
+ * @type {import('electron-builder').Configuration}
+ */
+const config = {
+  appId: 'com.novelbox.app',
+  productName: 'NovelBox',
+  directories: {
+    output: 'dist'
+  },
+  files: [
+    'dist/**/*',
+    'main.js',
+    'preload.js',
+    'package.json',
+    '!node_modules/**/*'
+  ],
+  extends: null,
+  win: {
+    target: [
+      {
+        target: 'nsis',
+        arch: ['x64']
+      },
+      {
+        target: 'zip',
+        arch: ['x64']
+      }
+    ]
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+    perMachine: true
+  },
+  mac: {
+    target: [
+      {
+        target: 'dmg',
+        arch: ['x64', 'arm64']
+      },
+      {
+        target: 'zip',
+        arch: ['x64', 'arm64']
+      }
+    ],
+    category: 'public.app-category.utilities'
+  },
+  linux: {
+    target: [
+      {
+        target: 'AppImage',
+        arch: ['x64']
+      },
+      {
+        target: 'deb',
+        arch: ['x64']
+      }
+    ],
+    category: 'Utility'
+  }
+}
+
+module.exports = config
